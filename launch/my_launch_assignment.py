39b869cbc9e1ae7f5361f1f4afcec3d02fc985c4
xxxxxChristian02xxxxx <negcris02@gmail.com>
2025-12-26 15:04:23 +0100

    added new world

--- launch/my_launch_assignment.py
+++ launch/my_launch_assignment.py
@@ -8,7 +8,7 @@ from launch.substitutions import  LaunchConfiguration, PathJoinSubstitution, Tex
 
 def generate_launch_description():
 	world_arg = DeclareLaunchArgument(
-		'world', default_value='my_world_assignment2.sdf',
+		'world', default_value='my_world_assignment.sdf',
 		description='Name of the Gazebo world file to load'
 	)
 
